d9dcbe169f6357228a698961af277d0d570226d2
hardeep1601 <hardeep1601@gmail.com>
2021-04-20 13:23:41 +0800

    Commit Shortest Hub Calulation

--- problem1.py
+++ problem1.py
@@ -1,49 +1,118 @@
 import gmplot
 
-# Create the map plotter:
-apikey = '' # (your API key here)
-gmap = gmplot.GoogleMapPlotter(3.519863, 101.538116, 14, apikey=apikey)
+class MapGenerate:
+    # Initialise hubs
+    courier_name = [
+        'City-link Express',
+        'Pos Laju',
+        'GDEX',
+        'J&T',
+        'DHL'
+    ]
+    hub_name = [
+        'Port Klang',
+        'Petaling Jaya',
+        'Batu Caves',
+        'Kajang',
+        'Sungai Buloh'
+    ]
+    hub_coordinate_x = [
+        3.0319924887507144,
+        3.112924170027219,
+        3.265154613796736,
+        2.9441205329488325,
+        3.2127230893650065
+    ]
+    hub_coordinate_y = [
+        101.37344116244806,
+        101.63982650389863,
+        101.68024844550233,
+        101.7901521759029,
+        101.57467295692778
+    ]
 
-#Initialise hubs
-hub_name = [
-    'Port Klang',
-    'Petaling Jaya',
-    'Batu Caves',
-    'Kajang',
-    'Sungai Buloh'
-]
-hub_coordinate_x = [
-    3.0319924887507144,
-    3.112924170027219,
-    3.265154613796736,
-    2.9441205329488325,
-    3.2127230893650065
-]
-hub_coordinate_y =[
-    101.37344116244806,
-    101.63982650389863,
-    101.68024844550233,
-    101.7901521759029,
-    101.57467295692778
-]
+    customer_origin = []
+    customer_destination = []
+    shortest_path = []
+    shortest_name = []
+    shortest_index = []
 
-# Mark hub locations
-for i in range(len(hub_name)):
-    gmap.text(hub_coordinate_x[i], hub_coordinate_y[i], hub_name[i])
-    gmap.marker(hub_coordinate_x[i], hub_coordinate_y[i], color='cornflowerblue')
+    # Create the map plotter:
+    apikey = ''  # (your API key here)
+    gmap = gmplot.GoogleMapPlotter(3.112924170027219, 101.63982650389863, 14, apikey=apikey)
+
+    def __init__(self):
+        # Mark hub locations
+        for i in range(len(self.hub_name)):
+            self.gmap.text(self.hub_coordinate_x[i], self.hub_coordinate_y[i], self.hub_name[i])
+            # gmap.marker(hub_coordinate_x[i], hub_coordinate_y[i], color='cornflowerblue')
+
+        self.gmap.scatter(self.hub_coordinate_x, self.hub_coordinate_y, 'red', size=400, marker=True)
 
 
+    def save_Customer(self, number):
+        # n = int(input('Enter the number of customers:'))
+        for i in range(number):
+            print('Customer ', i + 1)
+            self.customer_origin.append(input('Origin : '))
+            self.customer_destination.append(input('Destination : '))
+
+
+    def get_Shortest_Path(self):
+        map = test1.MapDistance()
+        print('------Suggest least distance for parcel to travel------')
+        # Calculate the least distance for parcel to travel for each customer
+        for i in range(len(self.customer_origin)):
+            shortest = 999999999999
+            shortestName = ''
+            # index
+            print('\n-----------Customer ', i+1, '-----------')
+            print('Calculating', end="")
+
+            # Calculate the closest hub for based on origin and destination
+            for j in range(len(self.hub_name)):
+                print('.', end="")
+                total_dist = map.calc_total_distance(self.customer_origin[i], self.customer_destination[i], self.hub_name[j])
+                # print('Hub name: ', self.hub_name[j])
+                # print('Total distance: ', total_dist)
+                if total_dist < shortest:
+                    shortest = total_dist
+                    index = j
+                    shortestName = self.hub_name[j]
+                # print('Shortest path: ', shortest)
+            self.shortest_name.append(shortestName)
+            self.shortest_path.append(shortest)
+            self.shortest_index.append(index)
+
+            # self.shortest_path[i]= total_dist
+            # Print the details
+            print('\n-----------Print shortest path-----------')
+            print(self.customer_origin[i], ' ---> ', self.shortest_name[i], ' ---> ', self.customer_destination[i])
+            print('Shortest hub name: ', self.shortest_name[i])
+            # print('Shortest hub name: ', self.hub_name[self.shortest_index[i]])
+            print('Recommended courier company: ', self.courier_name[self.shortest_index[i]])
+            print('Shortest distance: ', self.shortest_path[i])
+
+
+    def draw_HTML(self):
+        # Create the final map in HTML
+        self.gmap.draw('map.html')
+
+# End class
+
+# Main Class
+# Draw line and calculate distance of start and end point crossing the hubs
+import test1
+
+
+mapGen = MapGenerate()
+
+
+mapGen.save_Customer(int(input('Enter the number of customers:')))
+mapGen.get_Shortest_Path()
+mapGen.draw_HTML()
 
 
 
-# Outline the Distance between locations:
-distance = zip(*[
-    (3.0319924887507144, 101.37344116244806 ),
-    (3.3615395462207878, 101.56318183511695),
-    (3.1000170516638885, 101.53071480907951)
-])
 
-gmap.polygon(*distance, color='cornflowerblue', edge_width=10)
-# gmap.directions()
-print(distance)
-gmap.draw('map.html')
+
--- test1.py
+++ test1.py
@@ -5,6 +5,9 @@ from geopy.extra.rate_limiter import RateLimiter
 from geopy import distance
 
 
+# Reference
+# https://github.com/groundhogday321/python-geocoding-distance-measurement-with-geopy/blob/master/Python%20Geocoding%20%26%20Distance%20Measurement%20with%20GeoPy.ipynb
+# https://www.youtube.com/watch?v=3jj_5kVmPLs
 # import gmplot
 # apikey = '' # (your API key here)
 # gmap = gmplot.GoogleMapPlotter(3.519863, 101.538116, 14, apikey=apikey)
@@ -41,34 +44,56 @@ from geopy import distance
 #
 # gmap.draw('map.html')
 
-# Reference
-# https://github.com/groundhogday321/python-geocoding-distance-measurement-with-geopy/blob/master/Python%20Geocoding%20%26%20Distance%20Measurement%20with%20GeoPy.ipynb
 
 class MapDistance:
 
-    def calculate_distance(self, origin, destination):
-        for i in range(len(origin)):
-            pointA = Nominatim(user_agent='tutorial').geocode(origin[i])
-            pointB = Nominatim(user_agent='tutorial').geocode(destination[i])
-            print(origin[i], ' ---> ', destination[i])
-            print(
-                distance.distance((pointA.latitude, pointA.longitude), (pointB.latitude, pointB.longitude)).kilometers,
-                'km')
+    # Calculate the distance of 2 points and return a double
+    def calculate_distance(self, origin, dest):
+        # print('Calculating distance ...')
+        # for i in range(len(origin)):
+        pointA = Nominatim(user_agent='tutorial').geocode(origin)
+        pointB = Nominatim(user_agent='tutorial').geocode(dest)
+        # print('Customer ', i+1, ' :')
+        # print(origin, ' ---> ', dest)
+        hold_dist = round(distance.distance((pointA.latitude, pointA.longitude), (pointB.latitude, pointB.longitude)).kilometers, 2)
+        # print(hold_dist, 'km')
+        return hold_dist
 
-map = MapDistance
-n = int(input('Enter the number of customers:'))
-customer_origin = []
-customer_destination = []
+    # Returns coordinates and other details of a location based on 'name'
+    def get_Location(self, location):
+        return Nominatim(user_agent='tutorial').geocode(location)
 
-for i in range(n):
-    print('Customer ',i+1)
-    customer_origin.append(input('Origin : '))
-    customer_destination.append(input('Destination : '))
+    def calc_total_distance(self, start, end, hub_location):
+        map = MapDistance()
+        # print('Calculating distance ...')
+        pointA = Nominatim(user_agent='tutorial').geocode(start)
+        pointB = Nominatim(user_agent='tutorial').geocode(end)
+        pointC = Nominatim(user_agent='tutorial').geocode(hub_location)
+        # print(start, ' ---> ', hub_location, ' ---> ', end)
+        # hold_dist_1 = round(distance.distance((pointA.latitude, pointA.longitude), (pointB.latitude, pointB.longitude)).kilometers, 2)
+        # hold_dist_2 = round(distance.distance((pointA.latitude, pointA.longitude), (pointB.latitude, pointB.longitude)).kilometers, 2)
+        total_dist = map.calculate_distance(start, hub_location) + map.calculate_distance(hub_location, end)
+        # print(total_dist, 'km')
+        return round(total_dist, 2)
 
-print(customer_origin)
-print(customer_destination)
 
-map.calculate_distance(customer_origin, customer_destination)
+# Main Methods
+
+# map = MapDistance()
+# n = int(input('Enter the number of customers:'))
+# customer_origin = []
+# customer_destination = []
+
+# for i in range(n):
+#     print('Customer ',i+1)
+#     customer_origin.append(input('Origin : '))
+#     customer_destination.append(input('Destination : '))
+# map.calculate_distance(customer_origin, customer_destination)
+
+
+# print(customer_origin)
+# print(customer_destination)
+
 
 
 # indy_500_address = 'Rawang'
