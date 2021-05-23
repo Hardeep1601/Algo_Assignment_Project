aca74480a2d2a74d599e7445f8d0065d38d9d9e2
hardeep1601 <hardeep1601@gmail.com>
2021-05-23 20:59:29 +0800

    Commit Window Prompt 9

--- main (1).py
+++ main (1).py
@@ -9,8 +9,8 @@ distArr = []
 shortestArr = []
 fileArr = []
 
-print(w.originArr)
-print(w.destinationArr)
+# print(w.originArr)
+# print(w.destinationArr)
 
 t = parent()
 
@@ -21,48 +21,22 @@ for i in range(int(w.numOfCustomer)):
     t.setCustNo(w.customerID)
     t.setOrigin(w.originArr[i])
     t.setDest(w.destinationArr[i])
-    hold = t.getShort()
-    print('Hold : ', hold)
-    index = hold+(i*5)
-    # index = hold
-    # holdTime = timeArr[]
+    # hold = t.getShort()
+    # print('Hold : ', hold)
+    # index = hold+(i*5)
+
 
     fileArr.append(t.getDirectory())
     print(t.getJourneyTime())
     print(t.getDistance())
-    # t.clearArr()
     print('t.getShort() : ', t.getShort())
-    # w.htmlToImage()
     print("Run html to image")
-    # print('Route distance: ', t.routeDis()[1])
-    w.ouputWindow(t.getDistance(), t.getJourneyTime(), t.getShort(), t.routeDis()[0], t.routeDis()[1])
-    print('test')
-    # Both the distance array are the same for 2 different points
-    # print(distArr)
-    # print(shortestArr)
-
-# Output window
-# Problem: only got one customer HTML file cuz of only 1 directory
-# Problem 2: File open straight away after window launches
-w.saveFileName(fileArr)
+    w.saveDetails(t.getDistance(), t.getJourneyTime(), t.getShort(), t.routeDis()[0], t.routeDis()[1])
+    w.mainWindow()
+    # w.ouputWindow()
+    # print('test')
 
 
-# print(distArr)
-# print(shortestArr)
-# # Set Origin
-# t.setOrigin('Bukit Jelutong')
-# # Set Destination
-# t.setDest('Ampang Jaya')
-# # returns all journey time
-# print(t.getJourneyTime())
-# # returns all distances
-# print(t.getDistance())
-# # return the index of shortest time (used to get shortest path from JourneyTime and Distance list)
-# print(t.getShort())
-# # returns the file location
-# print(t.getDirectory())
 
+# w.saveFileName(fileArr)
 
-# a=[2,3,4,5,2]
-#
-# print(a.index(2))
--- windowPrompt3.py
+++ windowPrompt3.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import *
+from calcDirection import parent
 import imgkit
 from tkinter import filedialog
 
@@ -16,17 +17,70 @@ class Window:
     htmlFile = []
     customerID = 0
 
+    shortestIndex = 0
+    # For hubs
+    timeArr = []
+    distanceArr = []
+    # For door to door
+    distance2point = 0
+    time2point = 0
+
+
+
+
+
+
+
+    def mainWindow(self):
+        new_window = ''
+
+        def openMain():
+            # global new_window
+            new_window = Toplevel(root)
+            new_window.geometry("250x250")
+            new_window.title("New Window")
+            new_window.resizable(False, False)
+            lbl = Label(new_window, text='I am a new window')
+            lbl.pack()
+            btn2 = Button(new_window, text='Close Me', command=lambda: new_window.destroy())
+            btn2.pack()
+
+        root = Tk()
+        lbl = Label(new_window, text='Customer Delivery App')
+        lbl.pack()
+        bt = Button(root, text='Enter new customer', command=self.inputCustomer)
+        bt.pack(padx=10, pady=10)
+        bt = Button(root, text='Distance Details', command=self.ouputDistance)
+        bt.pack(padx=10, pady=10)
+        bt = Button(root, text='Sentiment Details', command=openMain)
+        bt.pack(padx=10, pady=10)
+
+        Button(root, text='Close Newly Opened Window', command=lambda: new_window.destroy()).pack()
+
+        root.geometry("500x500")
+        root.title('Main Window')
+        root.mainloop()
+
 
     def inputCustomer(self):
 
         first = tk.Tk()
         first.title("Customer Details")
-        tk.Label(first, text='State the ID of customer').grid(row=0, column=1)
+        # tk.Label(first, text='State the ID of customer').grid(row=0, column=1)
         tk.Label(first, text="Customer ID: ").grid(row=1)
 
         # type box
         box = tk.Entry(first)
         box.grid(row=1, column=1)
+
+        # tk.Label(first, text=str).grid(row=0, column=1)
+        tk.Label(first, text="Origin: ").grid(row=2)
+        tk.Label(first, text="Destination: ").grid(row=3)
+
+        origin = tk.Entry(first)
+        dest = tk.Entry(first)
+        origin.grid(row=2, column=1)
+        dest.grid(row=3, column=1)
 
 
         # STORE CUSTOMER ID AND FILE INFO
@@ -35,78 +89,67 @@ class Window:
             hold=box.get()
             # self.numOfCustomer = hold
             self.customerID = hold
+            self.originArr.append(origin.get())
+            self.destinationArr.append(dest.get())
             first.destroy()
 
+            ###################
+            # Clear all the array so that the correct values will be displayed
+
+
+
+
+
+
+            # Calculate the distance after pressing the submit button
+            t = parent()
+            t.setCustNo(self.customerID)
+            t.setOrigin(self.originArr[0])
+            t.setDest(self.destinationArr[0])
+            print(t.getJourneyTime())
+            print(t.getDistance())
+            print('t.getShort() : ', t.getShort())
+            print("Run html to image")
+            self.saveDetails(t.getDistance(), t.getJourneyTime(), t.getShort(), t.routeDis()[0], t.routeDis()[1])
+
+
+
         # button widget
-        b1 = Button(first, text="Submit", command=saveCustNum).grid(row=3, column=1, sticky=E)
+        b1 = Button(first, text="Submit", command=saveCustNum).grid(row=4, column=1, sticky=E)
 
         first.mainloop()
 
 
-        # GET CUSTOMER DETAILS
-        # Get the origin and destination for each customer
-        for i in range(1):
-            first = tk.Tk()
-            str='Customer Details'
-            first.title("Customer Details")
-            tk.Label(first, text=str).grid(row=0, column=1)
-            tk.Label(first, text="Origin: ").grid(row=1)
-            tk.Label(first, text="Destination: ").grid(row=2)
 
 
-            origin = tk.Entry(first)
-            dest = tk.Entry(first)
-            origin.grid(row=1, column=1)
-            dest.grid(row=2, column=1)
 
-            def saveCustDetail():
-                self.originArr.append(origin.get())
-                self.destinationArr.append(dest.get())
-                first.destroy()
 
-            # button widget
-            b1 = Button(first, text="Submit", command=saveCustDetail).grid(row=3, column=1,sticky = E)
-            first.mainloop()
+
+    def saveDetails(self, distanceArr, timeArr, shortestIndex, distance2point, time2point):
+        self.shortestIndex = shortestIndex
+        self.distance2point = distance2point
+        self.time2point = time2point
+        self.timeArr = timeArr
+        self.distanceArr = distanceArr
+
+
+
 
 
     # Create output window for program
-    def ouputWindow(self, distanceArr, timeArr, shortestIndex, distance2point, time2point):
+    def ouputDistance(self):
         root = tk.Tk()
 
         # Adjust size
         root.geometry("800x600")
         root.title('Customer Delivery Details')
 
-        # Change the label text
-        def show(*args):
-            getIndex = options.index(clicked.get())
-            # shortestIndex = shortestTimeArr[int(getIndex)]
-            # print('Test ;', shortestIndex)
-            # shortestHold = shortestIndex + getIndex*5
-            labelOrigin.config(text="\nOrigin : " + self.originArr[getIndex])
-            labelDestination.config(text="\nDestination : " + self.destinationArr[getIndex])
-            labelTime.config(text="\nTime taken : " + timeArr[shortestIndex])
-            label2.config(text="\nDistance between 2 points : " + distanceArr[shortestIndex])
 
-
-        # Dropdown menu options
-        # options = []
-        # for i in range(5):
-        #     options.append('Customer ' + str(i + 1))
-        #
-        # optionsFee = []
-        # for i in range(5):
-        #     optionsFee.append('Customer Fee ' + str(i + 1))
 
         header = Label(root, text="Customer Delivery Details\n")
         header.config(font=('Times', 14))
         header.pack()
 
-        # Create Dropdown menu
-        # clicked = StringVar(value="Select a hub")
-        # clicked.trace("w", show)
-        # drop = OptionMenu(root, clicked, *options)
-        # drop.pack()
 
         # Create Label/String to be attached
         labelID = Label(root, text="\nCustomer ID: "+ self.customerID)
@@ -117,9 +160,9 @@ class Window:
         labelOrigin.pack()
         labelDestination = Label(root, text="\nDestination : " + self.destinationArr[0])
         labelDestination.pack()
-        label2 = Label(root, text="\nDistance between 2 points : " + distance2point)
+        label2 = Label(root, text="\nDistance between 2 points : " + str(self.distance2point))
         label2.pack()
-        labelTime = Label(root, text="\nTime taken : " + time2point)
+        labelTime = Label(root, text="\nTime taken : " + str(self.time2point))
         labelTime.pack()
 
         # CREATE HUB DROPDOWN
@@ -140,8 +183,8 @@ class Window:
         # clicked1.set('Select a hub')
         def showHub(*args):
             getIndex = optionsHub.index(clicked1.get())
-            labelHub.config(text="\nDistance between 3 points : " + str(distanceArr[getIndex]) + ' km')
-            labelTime2.config(text="\nTime taken : " + str(timeArr[getIndex]) + ' min')
+            labelHub.config(text="\nDistance between 3 points : " + str(self.distanceArr[getIndex]) + ' km')
+            labelTime2.config(text="\nTime taken : " + str(self.timeArr[getIndex]) + ' min')
 
         clicked1 = StringVar(value="Select a hub")
         clicked1.trace("w", showHub)
@@ -153,7 +196,7 @@ class Window:
         labelHub.pack()
         labelTime2 = Label(root, text="\nTime taken :\t--")
         labelTime2.pack()
-        labelFastest = Label(root, text="\nFastest Hub : " + optionsHub[shortestIndex])
+        labelFastest = Label(root, text="\nFastest Hub : " + optionsHub[self.shortestIndex])
         labelFastest.pack()
 
         # INSERT the HTML file name for each customer
@@ -161,58 +204,8 @@ class Window:
 
         # Execute tkinter
         root.mainloop()
-        # End Window Create and Config
-        # ---------------------------------------------------
-        #
 
-        # Customer Details Output lines
-        # for i in range(int(self.numOfCustomer)):
-        #     bHeight = 80 + (i * 80)
-        #     custStr = 'Customer %d' % (i+1)
-        #
-        #     # Customer number
-        #     custLabel = tk.Label(root, text=(custStr+' : '))
-        #     custLabel.config(font=('helvetica', 12))
-        #     # canvas1.create_window(100, bHeight, window=custLabel)
-        #
-        #     # Show DISTANCE for each customer
-        #     # strDistance = '20.00 km'
-        #     distLabel = tk.Label(root, text=distanceArr[i])
-        #     distLabel.config(font=('helvetica', 12))
-        #     # canvas1.create_window(200, bHeight, window=distLabel)
-        #
-        #
-        #     # Show origin to destination POINTS
-        #     # Get the Origin and destination
-        #     pointsStr = '%s  ---->  %s' % (self.originArr[i], self.destinationArr[i])
-        #     pointsLabel = tk.Label(root, text=pointsStr)
-        #     pointsLabel.config(font=('helvetica', 12))
-        #     # canvas1.create_window(200, bHeight+40, window=pointsLabel)
-        #
-        #
-        #
-        #     # BUTTON to open file
-        #     # INSERT the HTML file name for each customer
-        #     b = Button(root, text=custStr + ' Map', command=self.openHTML(i))
-        #     # canvas1.create_window(500, bHeight, window=b)
 
-    # Save HTML to image - use IMGKIT python
-    # def htmlToImage(self):
-    #     path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    #     # config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-    #     config = pdfkit.configuration(wkhtmltopdf=bytes(path_wkhtmltopdf, 'utf-8'))
-    #
-    #
-    #     fileName = 'cusTest' + str(self.customerID) + '.jpg'
-    #     print(fileName)
-    #     imgkit.from_file(r'C:\Users\harde\Documents\Algo Assignment Project\cus1.html', 'cusTest.jpg', config=config)
-    #     # with open('cus1.html') as f:
-    #     #     imgkit.from_file(f, fileName)
-
-    # def saveFileName(self, fileName):
-    #     for i in range(len(fileName)):
-    #         nameStr = r'C:\Users\harde\Documents\Algo Assignment Project\\' + fileName[i]
-    #         self.htmlFile.append(nameStr)
 
     def openHTML(self, num):
         # nameStr = 'C:\Users\harde\Documents\Algo Assignment Project\\' +fileName
